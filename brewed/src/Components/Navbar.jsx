4eb548e5da991ac9175593b02b1c85ae051f55fc
AdieDev0 <devutella@gmail.com>
2025-01-12 22:21:50 +0800

    Import Image

--- brewed/src/Components/Navbar.jsx
+++ brewed/src/Components/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { IoPersonCircleOutline } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <div className="flex justify-between">
+        {/* LOGO */}
+        <img src="" alt="" />
+        <div className="flex gap-2">
+          <h1>Home</h1>
+          <h1>About</h1>
+          <h1>Coffees</h1>
+          <h1>Shop</h1>
+          <h1>Blog</h1>
+          <h1>Contact</h1>
+        </div>
+        {/* Login */}
+        <button className="flex">
+          <IoPersonCircleOutline />
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
