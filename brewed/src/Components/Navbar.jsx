d859404873157f1b03dcf131f346de065b3a4019
AdieDev0 <devutella@gmail.com>
2025-01-12 22:32:07 +0800

    Added Font

--- brewed/src/Components/Navbar.jsx
+++ brewed/src/Components/Navbar.jsx
@@ -1,25 +1,38 @@
 import React from "react";
 import { IoPersonCircleOutline } from "react-icons/io5";
-
+import { LuSearch } from "react-icons/lu";
+import Logo from "../assets/LogoNav.png";
 const Navbar = () => {
   return (
-    <div>
-      <div className="flex justify-between">
+    <div className="px-28">
+      <div className="flex justify-between items-center">
         {/* LOGO */}
-        <img src="" alt="" />
-        <div className="flex gap-2">
-          <h1>Home</h1>
-          <h1>About</h1>
-          <h1>Coffees</h1>
-          <h1>Shop</h1>
-          <h1>Blog</h1>
-          <h1>Contact</h1>
+        <div className="flex items-center">
+          <img src={Logo} alt="/" className="size-28" />
+          <h1>BREW HAVEN</h1>
+        </div>
+        <div className="flex gap-16">
+          <h1 className="font-BarlowCondensed text-2xl cursor-pointer">Home</h1>
+          <h1 className="font-BarlowCondensed text-2xl cursor-pointer">
+            About
+          </h1>
+          <h1 className="font-BarlowCondensed text-2xl cursor-pointer">
+            Coffees
+          </h1>
+          <h1 className="font-BarlowCondensed text-2xl cursor-pointer">Shop</h1>
+          <h1 className="font-BarlowCondensed text-2xl cursor-pointer">Blog</h1>
+          <h1 className="font-BarlowCondensed text-2xl cursor-pointer">
+            Contact
+          </h1>
         </div>
         {/* Login */}
-        <button className="flex">
-          <IoPersonCircleOutline />
-          Login
-        </button>
+        <div className="flex gap-10 items-center">
+          <button className="flex items-center text-xl font-medium gap-1">
+            <IoPersonCircleOutline />
+            Login
+          </button>
+          <LuSearch className="text-xl cursor-pointer" />
+        </div>
       </div>
     </div>
   );
