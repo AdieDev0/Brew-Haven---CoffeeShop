886b49176ee10fc10321968ecc0169f1c5775c57
AdieDev0 <devutella@gmail.com>
2025-01-12 22:08:50 +0800

    Added Pages

--- brewed/src/App.jsx
+++ brewed/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Shop from "./Pages/Shop";
+import Blog from "./Pages/Blog";
+import Contact from "./Pages/Contact";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ brewed/src/Pages/Coffees.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Coffees = () => {
+  return (
+    <div>Coffee</div>
+  )
+}
+
+export default Coffees
