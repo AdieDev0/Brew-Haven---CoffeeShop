d550c043ea511b7e6ac35eced766c74ef33cd5ce
AdieDev0 <devutella@gmail.com>
2025-01-14 15:58:03 +0800

    Coffee Blog

--- brewed/src/Components/OurBlog.jsx
+++ brewed/src/Components/OurBlog.jsx
@@ -1,65 +1,65 @@
-    import React from "react";
-    import CoffeeGuy from "../assets/OurBlog/CoffeeGuy.jpg";
-    import Espresso from "../assets/OurBlog/espresso.jpg";
+import React from "react";
+import CoffeeGuy from "../assets/OurBlog/CoffeeGuy.jpg";
+import Espresso from "../assets/OurBlog/espresso.jpg";
 
-    const OurBlog = () => {
-    return (
-        <div className="bg-gray-100 py-16 px-5 lg:px-60">
-        <h1 className="font-Kanit text-4xl text-gray-800 font-semibold mb-10">
-            OUR BLOG
-        </h1>
-        {/* 2 CARDS */}
-        <div className="flex gap-5">
-            {/* CARDS */}
-            <div className="text-center">
-            <div className="shadow-lg rounded-xl overflow-hidden">
-                <img
-                src={CoffeeGuy}
-                alt="Coffee Preparation"
-                className="w-full lg:w-[500px] object-cover"
-                />
-                <div className="p-5 bg-white w-[500px]">
-                <h1 className="font-Kanit font-semibold text-start text-2xl text-gray-900">
-                    PREP TECHNIQUES Coffee
-                </h1>
-                <p className="font-Kanit text-start text-gray-700 mt-3">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Cupiditate eos voluptate officiis sunt, adipisci repellendus
-                    earum laudantium accusantium aspernatur eaque ex nesciunt. Fugit
-                    quo delectus accusamus quaerat quas natus iste.
-                </p>
-                </div>
-            </div>
-            <button className="text-red-600 text-xs md:text-sm lg:text-base w-52 mt-5 font-Kanit border border-red-600 hover:text-black hover:border-black duration-200 rounded-lg px-4 md:px-6 lg:px-8 py-2 md:py-3 lg:py-4">
-                READ MORE
+const OurBlog = () => {
+  return (
+    <div className="bg-gray-100 py-16 px-5 lg:px-20">
+      <h1 className="font-Kanit text-3xl md:text-4xl text-gray-800 font-semibold mb-10 text-center">
+        OUR COFFEE BLOG
+      </h1>
+      {/* Blog Cards */}
+      <div className="flex flex-wrap gap-5 justify-center">
+        {/* Card 1 */}
+        <div className="max-w-sm shadow-lg rounded-xl overflow-hidden bg-white">
+          <img
+            src={CoffeeGuy}
+            alt="Coffee Preparation"
+            className="w-full h-48 object-cover"
+          />
+          <div className="p-5">
+            <h2 className="font-Kanit font-semibold text-xl text-gray-900">
+              Brewing the Perfect Cup
+            </h2>
+            <p className="font-Kanit text-gray-700 mt-3 text-sm md:text-base">
+              Discover tips and techniques to brew the perfect cup of coffee at
+              home. From grind size to brewing time, we cover all the essentials
+              for a rich and flavorful experience.
+            </p>
+          </div>
+          <div className="p-5">
+            <button className="text-red-600 text-sm md:text-base w-full font-Kanit border border-red-600 hover:text-white hover:bg-red-600 duration-200 rounded-lg py-2">
+              READ MORE
             </button>
-            </div>
-            <div className="text-center">
-            <div className="shadow-lg rounded-xl overflow-hidden">
-                <img
-                src={Espresso}
-                alt="Coffee Preparation"
-                className="w-full lg:w-[500px] h-[333px] object-cover"
-                />
-                <div className="p-5 bg-white w-[500px]">
-                <h1 className="font-Kanit font-semibold text-start text-2xl text-gray-900">
-                    PREP TECHNIQUES Coffee
-                </h1>
-                <p className="font-Kanit text-start text-gray-700 mt-3">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Cupiditate eos voluptate officiis sunt, adipisci repellendus
-                    earum laudantium accusantium aspernatur eaque ex nesciunt. Fugit
-                    quo delectus accusamus quaerat quas natus iste.
-                </p>
-                </div>
-            </div>
-            <button className="text-red-600 text-xs md:text-sm lg:text-base w-52 mt-5 font-Kanit border border-red-600 hover:text-black hover:border-black duration-200 rounded-lg px-4 md:px-6 lg:px-8 py-2 md:py-3 lg:py-4">
-                READ MORE
-            </button>
-            </div>
+          </div>
         </div>
-        </div>
-    );
-    };
 
-    export default OurBlog;
+        {/* Card 2 */}
+        <div className="max-w-sm shadow-lg rounded-xl overflow-hidden bg-white">
+          <img
+            src={Espresso}
+            alt="Espresso Preparation"
+            className="w-full h-48 object-cover"
+          />
+          <div className="p-5">
+            <h2 className="font-Kanit font-semibold text-xl text-gray-900">
+              Mastering Espresso Techniques
+            </h2>
+            <p className="font-Kanit text-gray-700 mt-3 text-sm md:text-base">
+              Learn the art of making espresso like a pro. Explore tips on
+              tamping, extraction time, and achieving the perfect crema for
+              every shot.
+            </p>
+          </div>
+          <div className="p-5">
+            <button className="text-red-600 text-sm md:text-base w-full font-Kanit border border-red-600 hover:text-white hover:bg-red-600 duration-200 rounded-lg py-2">
+              READ MORE
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurBlog;
