ca38aadcf8073f6382fa823c5949e2b30e85fbec
AdieDev0 <devutella@gmail.com>
2025-01-14 19:18:10 +0800

    MAP API

--- brewed/src/Components/MapApi.jsx
+++ brewed/src/Components/MapApi.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect } from "react";
+
+const containerStyle = {
+  width: "100%",
+  height: "400px",
+};
+
+const centerCoordinates = {
+  lat: 14.540867, // Latitude
+  lng: 121.050316, // Longitude
+};
 
 const MapApi = () => {
-  return (
-    <div>MapApi</div>
-  )
-}
+  useEffect(() => {
+    // Load Google Maps script
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDz6Ksxzwrk5KMWjqqF0og7P61qXKzitA4`;
+    script.async = true;
+    document.body.appendChild(script);
 
-export default MapApi
+    script.onload = () => {
+      // Initialize map
+      const map = new window.google.maps.Map(document.getElementById("map"), {
+        center: centerCoordinates,
+        zoom: 12,
+      });
+
+      // Add marker
+      new window.google.maps.Marker({
+        position: centerCoordinates,
+        map: map,
+      });
+    };
+
+    return () => {
+      // Cleanup
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div>
+      {/* Google Map Container */}
+      <div id="map" style={containerStyle} className="shadow-lg"></div>
+
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default MapApi;
