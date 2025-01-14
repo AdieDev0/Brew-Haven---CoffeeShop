3601f036915e6330bbb8d1b65fdbcc00e5c4392a
AdieDev0 <devutella@gmail.com>
2025-01-14 18:58:33 +0800

    GetInTouch Done

--- brewed/src/Components/GetInTouch.jsx
+++ brewed/src/Components/GetInTouch.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const GetInTouch = () => {
   return (
-    <div>GetInTouch</div>
-  )
-}
+    <div className="bg-gray-100 py-16 px-5 lg:px-60">
+      <div className="text-center">
+        <h1 className="text-4xl text-gray-800 font-bold font-Kanit mb-10">
+          GET IN TOUCH
+        </h1>
+        <div className="grid gap-3 md:gap-5 lg:gap-7 px-5 md:px-20 lg:px-44">
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="font-Kanit font-normal text-black placeholder-black/60 p-3 md:p-4 lg:p-5 border border-black/40 rounded-full"
+          />
+          <input
+            type="email"
+            placeholder="Your Email"
+            className="font-Kanit font-normal text-black placeholder-black/60 p-3 md:p-4 lg:p-5 border border-black/40 rounded-full"
+          />
+          <input
+            type="tel"
+            placeholder="Your Phone"
+            className="font-Kanit font-normal text-black placeholder-black/60 p-3 md:p-4 lg:p-5 border border-black/40 rounded-full"
+          />
+          <textarea
+            placeholder="Your Message"
+            rows="5"
+            className="font-Kanit font-normal text-black placeholder-black/60 p-3 md:p-4 lg:p-5 border border-black/40 rounded-lg resize-y"
+          ></textarea>
+        </div>
+        <button className="text-red-600 mt-10 text-xs md:text-sm lg:text-base w-32 md:w-40 lg:w-52 font-Kanit border border-red-600 hover:bg-red-600 hover:text-white hover:border-white duration-200 rounded-full px-4 md:px-6 lg:px-8 py-2 md:py-3 lg:py-4">
+          SEND
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default GetInTouch
+export default GetInTouch;
