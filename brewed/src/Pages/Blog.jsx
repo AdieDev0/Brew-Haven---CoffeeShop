6bd07c39f5d28d5ddd85317412ff11eb0238daaf
AdieDev0 <devutella@gmail.com>
2025-01-17 00:10:45 +0800

    Forgot Navbar

--- brewed/src/Pages/Blog.jsx
+++ brewed/src/Pages/Blog.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import OurBlog from "../Components/OurBlog";
 import BGimg from "../assets/BGimg.jpg";
+import Navbar from "../Components/Navbar";
 const Blog = () => {
   return (
     <div>
