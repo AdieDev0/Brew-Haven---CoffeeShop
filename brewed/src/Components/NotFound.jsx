a770e0c1616bd2c10c50f46e28d7437dda376b4e
AdieDev0 <devutella@gmail.com>
2025-01-16 22:37:09 +0800

    Fix bug

--- brewed/src/Components/NotFound.jsx
+++ brewed/src/Components/NotFound.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-
+import notFound from "../assets/404/NotFound.png";
 const NotFound = () => {
   return (
-    <div>
-      <img src="" alt="" />
-      NotFound
+    <div className="grid 5">
+      <img src={notFound} alt="" className="size-20" />
+      <h1 className="font-Kanit font-semibold">Not Found please back home.</h1>
+      <Navlink to="/">
+        <button>Home</button>
+      </Navlink>
     </div>
   );
 };
--- brewed/src/Pages/Shop.jsx
+++ brewed/src/Pages/Shop.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
+import React from "react";
+import NotFound from "../Components/NotFound";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const Shop = () => {
   return (
     <div>
-    <div
-      className="relative w-full h-[200px] md:h-[90px] bg-cover bg-center"
-      style={{ backgroundImage: `url(${BGimg})` }}
-    >
-      <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-md"></div>
+      <div
+        className="relative w-full h-[200px] md:h-[90px] bg-cover bg-center"
+        style={{ backgroundImage: `url(${BGimg})` }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-md"></div>
 
-      {/* Navbar */}
-      <div className="relative z-10">
-        <Navbar />
+        {/* Navbar */}
+        <div className="relative z-10">
+          <Navbar />
+        </div>
       </div>
+
+      {/* Main Content */}
+
+      <div className="mb-20">
+        <NotFound />
+      </div>
+      <Footer />
     </div>
+  );
+};
 
-    {/* Main Content */}
-
-    <div className="mb-20">
-      
-    </div>
-    <Footer />
-  </div>
-  )
-}
-
-export default Shop
+export default Shop;
