b53890ae063e7badc5ba2d6fef1daa72ad15468e
AdieDev0 <devutella@gmail.com>
2025-01-17 00:09:30 +0800

    Set up blog changing compo

--- brewed/src/Pages/Blog.jsx
+++ brewed/src/Pages/Blog.jsx
@@ -3,8 +3,25 @@ import React from "react";
 const Blog = () => {
   return (
     <div>
-      <div></div>
+    <div
+      className="relative w-full h-[65px] md:h-[90px] bg-cover bg-center"
+      style={{ backgroundImage: `url(${BGimg})` }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-md"></div>
+
+      {/* Navbar */}
+      <div className="relative z-10">
+        <Navbar />
+      </div>
     </div>
+
+    {/* Main Content */}
+
+    <div className="mb-20">
+      <CoffeeOffer />
+    </div>
+    <Footer />
+  </div>
   );
 };
 
