5f77f7e22555fd33fa516ad5ba0d00945d880b94
AdieDev0 <devutella@gmail.com>
2025-01-16 22:41:52 +0800

    NotFound

--- brewed/src/Components/NotFound.jsx
+++ brewed/src/Components/NotFound.jsx
@@ -8,13 +8,14 @@ const NotFound = () => {
       <img
         src={notFound}
         alt="404 Not Found"
-        className="w-full max-w-md mb-6"
+        className="w-full max-w-sm mb-6"
       />
       <h1 className="text-2xl md:text-3xl font-Kanit font-semibold text-gray-800 mb-4">
         Oops! Page Not Found
       </h1>
       <p className="text-gray-600 text-base md:text-lg mb-6">
-        The page you’re looking for doesn’t exist or has been moved. Please go back home.
+        The page you’re looking for doesn’t exist or has been moved. Please go
+        back home.
       </p>
       <NavLink to="/">
         <button className="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
